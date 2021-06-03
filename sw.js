665347ea8729fef00557823f537a229ed7075525
onionSequences <kirbi9@gmail.com>
2021-06-03 09:52:19 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d734d969de94905dbed7.js"
+    "url": "webpack-runtime-8df75b4ae25ab465ea64.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e9fabff1239fcd9be890f9d4f86e5b22"
+    "revision": "4fa8c33ba5d1bed7fdb04489da117d35"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e9787d58a08b099110305fd0143613fe"
+    "revision": "ed226867c5498fcf3a81507d4d5970b5"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
